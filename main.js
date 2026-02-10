7bfa818c1cec380963fcaba518678ac9d4e92ba1
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-10 21:10:20 +0530

    Defining the constant Variable

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-let a = 1;                // a is a Variable
-a = 2;
+const a = 1;                // a is a Variable
 console.log(a);           // Function
+
+// let : When the value is changing
+// Defining the values that wont change
