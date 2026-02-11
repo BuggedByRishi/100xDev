c45165bb7068ec56ddb5046a54e29322ef41660d
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-11 13:53:08 +0530

    Sum of two numbers using Functions

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-const allUser = [
-    {
-        name: "Rishi",
-        gender: "male"
-    },
-    {
-        name: "Riya",
-        gender: "female"
-    },
-    {
-        name: "Rohit",
-        gender: "male"
-    }]
-
-for(let i = 0; i < allUser.length; i++){
-    if(allUser[i]["gender"] == "male"){
-        console.log(allUser[i]["name"])
-    }
+function Sum(a,b){
+    return a+b;
 }
+
+const value = Sum(1,2);
+console.log(value);
