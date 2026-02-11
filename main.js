2c7455a5d0ecc26fcc49eac8649ae5b64e4d8496
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-11 13:25:01 +0530

    Using Array an to print Even Ages

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-let ans = 0;
+const ages = [32, 33, 16, 40];
+const numberofages = ages.length;
 
-for (let i=0; i<=100; i++) {
-  ans = ans + i;
+for(let i = 0; i < numberofages; i   ++)
+{
+    if(ages[i] % 2 == 0)
+    {
+        console.log(ages[i]);
+    }
 }
-
-console.log(ans);
