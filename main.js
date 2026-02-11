4399f79b448314af7f2b1731aa67993dc3c037ff
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-12 01:50:57 +0530

    Callback function fo rthe sum of two numbers

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-function Sum(a,b){
-    return a+b;
+function sum(num1, num2, fnToCall) {
+    let result = num1 + num2;
+    fnToCall(result);                           // This is a callback function that will be called with the result of the sum
 }
 
-const value = Sum(1,2);
-console.log(value);
+function displayResult(data) {
+    console.log("The result of the sum is: " + data);
+}
+
+function displayResultPassive(data) {
+    console.log("The sum's result is: " + data);
+}
+
+const ans = sum(5, 10, displayResult);          // Passed this function to the function sum, so that it can call it when it has the result of the sum. This is a callback function.
