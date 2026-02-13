ce43caad6d64595ce980df7bd3217c5b4c9c3fdc
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-13 17:44:04 +0530

    Spliting the String

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const value = "IF YOU ARE READING THIS, IT'S LATE";
-const words = value.split(" ");
+const words = value.split(" ");         // Returning the strings in the form of an array
 
 console.log(words);
