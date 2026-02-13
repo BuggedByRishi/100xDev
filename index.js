4ef27ebced9bccbe046e249bc666dff8a1ee568d
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-13 15:20:30 +0530

    Length of a String

---
+++ index.js
@@ -0,0 +1,6 @@
+function geetlength(str) {
+    console.log("Original string", str);
+    console.log("Length of the string", str.length);
+}
+
+geetlength("IF YOU ARE READING THIS, IT'S ALREADY LATE");
