099f4820472235c0b73d38415fc8d7e9aff6edf2
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-15 21:56:51 +0530

    Calling functin inside a function

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-function sum(n) {                       // Function to calculate Sum
-    let ans = 0;
-
-    for(let i = 0; i < n; i++) {
-        ans = ans+1;
-    }
-    return ans;                         // Returning the Sum
+function square(n) {
+    return n *n;
 }
 
-let demo = sum(100);                    // Calling the function nad storing the result in the demo variable
-console.log(demo);
+function sumOfSquares(a, b) {
+    const value1 = square(a);
+    const value2 = square(b);
 
-let demo2 = sum(1000);                  // Calling the function nad storing the result in the demo2 variable
-console.log(demo2);
-    
+    return value1 + value2;
+}
+
+console.log(sumOfSquares(3, 4));
