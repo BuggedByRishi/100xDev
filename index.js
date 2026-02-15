50152bb3fc9536ccc4c044e9718064143e315ff0
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-15 21:45:00 +0530

    Practicing Functions

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-let ans = 0; 
+function sum(n) {                       // Function to calculate Sum
+    let ans = 0;
 
-for(let i = 1; i <= 50; i++) {
-    ans = ans + i;
+    for(let i = 0; i < n; i++) {
+        ans = ans+1;
+    }
+    return ans;                         // Returning the Sum
 }
 
-console.log(ans);
+let demo = sum(100);                    // Calling the function nad storing the result in the demo variable
+console.log(demo);
+
+let demo2 = sum(1000);                  // Calling the function nad storing the result in the demo2 variable
+console.log(demo2);
+    
