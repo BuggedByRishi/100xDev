04f96300ab4341ae33c50041492d5fd3a7b923bf
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-17 02:36:15 +0530

    Promise function Example 1

--- Class5.js
+++ Class5.js
@@ -1,19 +1,12 @@
-const fs = require('fs');   // Using File system module to read and write files
+var d = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve("Foo");
+    }, 10000)
+})
 
-function rishiFile() {   
-    console.log("Inside rishiFile");                                                // Async functions
-    return new Promise(function(resolve) {
-        console.log("Inside Promise");
-        fs.readFile("a.txt", "utf-8", function(err, data) {
-            console.log("Before resolve");
-            resolve(data);
-        })
-    })
+function callback() {
+    console.log(d);
 }
 
-function onDone(data) {                 // Callback function to call the async function
-    console.log(data)
-}
-
-var a = rishiFile();   // Calling the async function and then calling the callback function on completion
-a.then(onDone);
+console.log(d);
+d.then(callback);
