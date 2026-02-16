f6dba9f0bdd1d6dcd84c42223fd9f47a23de81f0
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-17 00:11:29 +0530

    Async Function another Example

--- Class5.js
+++ Class5.js
@@ -1,8 +1,14 @@
 const fs = require("fs");
 // filesystem module
 
-fs.readFile("a.txt", "utf-8", function (err, data) {
-    console.log(data);
+fs.readFile("a.txt", "utf-8", function (err, data) {    // Asynchronous Read file function (an expensive function as this takes time to read a file)
+    console.log(data);  
 })
 
 console.log("Hi there");
+let a = 0;
+for(let i = 0; i < 100; i++) {
+    a++;
+}
+
+console.log("Bye there");
