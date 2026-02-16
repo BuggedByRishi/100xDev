57ed771fea694b34da2674f3eefbdadb3061d3f5
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-17 00:25:23 +0530

    Async Function Example 2

--- Class5.js
+++ Class5.js
@@ -1,12 +1,15 @@
 console.log("Hi there!");
 
-function calculateSum() {
+setTimeout(function() {
+    console.log("From the inside Async function");
+}, 2000);
+
     let a = 0;
 
     for (let i = 0; i < 5; i++) {
         a = a + i;
     }
     console.log(a);
-}
 
-calculateSum();
+
+console.log(a);
