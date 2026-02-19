2173f1afae618dcf4d2596123702ce9d5464f608
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-19 19:02:31 +0530

    Timeout Function

--- Class6.js
+++ Class6.js
@@ -1,17 +1,10 @@
-    function square(n) {
-        return n * n;
-    }
+function onDone() {
+    console.log("Hi there");    
+}
 
-    function cube(n) {
-        return n * n * n;
-    }
+setTimeout(onDone, 1000);
+console.log("After SetTimeout");        
 
-    // Generic function that takes a function as an argument
-    function sumOfSomething(a,b,fn) {       // Passing the function as an argument
-        let square1 = fn(a);
-        let square2 = fn(b);
-        return square1 + square2;
-    }
-
-    let ans = sumOfSomething(1, 2, square); 
-    console.log(ans);
+for(let i = 0; i < 100; i++){
+    console.log("Hellows");
+}
