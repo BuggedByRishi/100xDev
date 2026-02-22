af43a77f33ef2af465110f25c56f68f623eccaec
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-22 14:36:35 +0530

    Practicing Promise

--- Class6.js
+++ Class6.js
@@ -1,15 +1,27 @@
-let p = new Promise((resolve, reject) => {
-    let a = 1+2
-    if(a == 2){
-        resolve('Success')
-    }
-    else{
-        reject("Failed")
-    }
-})
+const userLeft = false;
+const userWatchingCatMeme = false;
 
-p.then((message) => {
-    console.log("This is the ",message);
-}) .catch((message) => {
-    console.log("This is the ",message)
+function userPromise() {
+    return new Promise((resolve, reject) => {
+        if(userLeft){
+            reject({
+                name : 'User Left',
+                message : ':('
+            })
+        }
+        else if(userWatchingCatMeme){
+            reject({
+                name: 'User watching Cat Meme',
+                message: 'WebSimplified > Cat'
+            })
+        }else {
+            resolve('Thumps up and Subscribe')
+        }
+    })
+}
+
+userPromise().then((message) => {
+    console.log('Success: ',message);
+}).catch((error) => {
+    console.log(error.name, '' , error.message)
 })
